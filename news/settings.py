f1dbe1ecd4e83147deafc2dafab9ae04b599c390
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-03-30 19:36:12 +0530

    fourth commit

--- news/settings.py
+++ news/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'summnews.apps.SummnewsConfig',
-    'travello2.apps.TravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
